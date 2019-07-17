b258786f455553a64e9996302b82e1f21ba26563
unknown <1041942517@qq.com>
2019-07-17 09:21:11 +0800

    first commit

---
+++ app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/FeedResponse.java
@@ -0,0 +1,21 @@
+package com.bytedance.android.lesson.restapi.solution.bean;
+
+import java.util.List;
+
+/**
+ * @author Xavier.S
+ * @date 2019.01.20 14:17
+ */
+public class FeedResponse {
+    List<Feed> feeds;
+    Boolean success;
+
+    public List<Feed> getFeeds() {
+        return feeds;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+    // TODO-C2 (2) Implement your FeedResponse Bean here according to the response json
+}
---
+++ app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/PostVideoResponse.java
@@ -0,0 +1,33 @@
+package com.bytedance.android.lesson.restapi.solution.bean;
+
+import org.json.JSONObject;
+
+/**
+ * @author Xavier.S
+ * @date 2019.01.18 17:53
+ */
+public class PostVideoResponse {
+    JSONObject result;
+    String url;
+    boolean success;
+    private Feed feed;
+    public boolean isResult() {
+        return success;
+    }
+
+    public Feed getFeed() {
+        return feed;
+    }
+    // TODO-C2 (3) Implement your PostVideoResponse Bean here according to the response json
+    // url: (POST) http://test.androidcamp.bytedance.com/mini_douyin/invoke/video?student_id={student_id}&user_name={user_name}
+    // body
+    // + cover_image，file
+    // + video, file
+    // response
+    // {
+    //    "result": {},
+    //    "url": "https://lf1-hscdn-tos.pstatp
+    //    .com/obj/developer-baas/baas/tt7217xbo2wz3cem41/a8efa55c5c22de69_1560563154288.mp4",
+    //    "success": true
+    //}
+}
